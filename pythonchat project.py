04cec4e06c99831fd4f0dfb0d366bafbba7f4ec3
Maikel Lin <31967699+maikellin@users.noreply.github.com>
2017-11-21 08:56:37 -0500

    Added files by upload - first commit

---
+++ pythonchat project.py
@@ -0,0 +1,11 @@
+def list_of_the_users:
+
+    if n == :
+        return "message sent."
+    else:
+        returnt "error: message not sent."
+    
+    
+users=("mikki, annie, stefenos, owen, tianhao, kayla, leo, justis, maikel, gen")
+n = input ("enter the username.")
+n = input ("please enter the message.")
